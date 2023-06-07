a6bd70b8c99edb5645f08939b3dd480231c25102
Source-Code-007 <utsho926@gmail.com>
2023-06-07 10:10:01 +0600

    auth func added from firebase and pass globally via context API

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBusWX6i8ct2GAKd5MZao4TxPpHe-UVNj4",
+  authDomain: "wizcraft-academy.firebaseapp.com",
+  projectId: "wizcraft-academy",
+  storageBucket: "wizcraft-academy.appspot.com",
+  messagingSenderId: "306171896699",
+  appId: "1:306171896699:web:be3e6c4a1814ab5ff313de"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,13 +1,76 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { app } from "../../firebase.config";
 
+const auth = getAuth(app)
 const authContextData = createContext()
-const AuthContext = ({children}) => {
+const googleProvider = new GoogleAuthProvider()
+const githubProvider = new GithubAuthProvider()
+
+const AuthContext = ({ children }) => {
+    const [user, setUser] = useState('')
+    const [authLoading, setAuthLoading] = useState(false)
+
+    // create user with email and password func
+    const createUserWithEmailPassFunc = (email, password) => {
+        setAuthLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // update profile info func
+    const updateProfileFunc = (name, photo) => {
+        setAuthLoading(true)
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        })
+    }
 
 
+
+    // signin user with email and password func
+    const signinUserWithEmailPassFunc = (email, password) => {
+        setAuthLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    // signin user with google func
+    const signinUserWithEmailFunc = () => {
+        setAuthLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    // signin user with github func
+    const signinUserWithGithubFunc = () => {
+        setAuthLoading(true)
+        return signInWithPopup(auth, githubProvider)
+    }
+
+    // signout user func
+    const signoutUserFunc = () => {
+        setAuthLoading(true)
+        return signOut(auth)
+    }
+
+
+    // user monitoring via onAuthStateChange
+    useEffect(() => {
+        const unsubscribe = () => {
+            onAuthStateChanged(auth, currUser => {
+                if (currUser) {
+                    setUser(currUser)
+                    setAuthLoading(false)
+                } else {
+                    setUser(currUser)
+                    setAuthLoading(false)
+                }
+            })
+        }
+        () => {
+            return unsubscribe()
+        }
+    }, [])
 
 
     const authContextObj = {
-        name: 'Utsho'
+        user, setUser, authLoading, setAuthLoading, createUserWithEmailAndPassword, signinUserWithEmailPassFunc, signinUserWithEmailFunc, signinUserWithGithubFunc
     }
     return (
         <authContextData.Provider value={authContextObj}>
