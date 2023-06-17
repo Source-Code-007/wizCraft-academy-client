2c731c81df2d92721f6a66ff628b60dde859606b
Source-Code-007 <utsho926@gmail.com>
2023-06-17 12:41:05 +0600

    gallery section added

--- src/Pages/Homepage/Gallery/Gallery.jsx
+++ src/Pages/Homepage/Gallery/Gallery.jsx
@@ -6,18 +6,30 @@ import galleryImg4 from '../../../assets/img/Gallery/gallery-3.jpg'
 
 const Gallery = () => {
     return (
-        <div className='my-container'>
+        <div className='my-container py-14'>
             <CommonSectionTitle title={'Magic in Frames'} subtitle={'A Captivating Gallery of Enchanting Moments'}></CommonSectionTitle>
-            <div className='grid grid-cols-12 gap-6'>
-                <div className='col-span-3 space-y-8'>
-                    <img src={galleryImg1} alt="" />
-                    <img src={galleryImg3} alt="" />
+            <div className='grid grid-cols-12 gap-7'>
+                <div className='col-span-12 lg:col-span-3 flex flex-col gap-7'>
+                    <div className="relative group !h-[500px] md:!h-full overflow-hidden">
+                        <img src={galleryImg1} className="h-full w-full object-cover transform group-hover:scale-105 transition duration-500" alt="" />
+                        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-50"></div>
+                    </div>
+                    <div className="relative group !h-[500px] md:!h-full overflow-hidden">
+                        <img src={galleryImg3} className="h-full w-full object-cover transform group-hover:scale-105 transition duration-500" alt="" />
+                        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-50"></div>
+                    </div>
                 </div>
-                <div className='col-span-6'>
-                    <img className='h-full' src={galleryImg2} alt="" />
+                <div className='col-span-12 lg:col-span-6'>
+                    <div className="relative group !h-[500px] md:!h-full overflow-hidden">
+                        <img src={galleryImg2} className="h-full w-full object-cover transform group-hover:scale-105 transition duration-500" alt="" />
+                        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-50"></div>
+                    </div>
                 </div>
-                <div className='col-span-3'>
-                    <img src={galleryImg4} alt="" />
+                <div className='col-span-12 lg:col-span-3'>
+                    <div className="relative group  !h-[500px] md:!h-full overflow-hidden">
+                        <img src={galleryImg4} className="h-full w-full object-cover transform group-hover:scale-105 transition duration-500" alt="" />
+                        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-50"></div>
+                    </div>
                 </div>
             </div>
         </div>
