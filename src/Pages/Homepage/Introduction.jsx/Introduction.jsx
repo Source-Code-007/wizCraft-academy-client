6c437911de49d4c1559c4bda0118719c0c53b64b
Source-Code-007 <utsho926@gmail.com>
2023-06-17 11:22:55 +0600

    video section added on homepage

--- src/Pages/Homepage/Introduction.jsx/Introduction.jsx
+++ src/Pages/Homepage/Introduction.jsx/Introduction.jsx
@@ -12,10 +12,10 @@ const Introduction = () => {
                 <h2 className='font-bold text-5xl'>Welcome to Camp of Summers</h2>
                 <p className='flex gap-3 items-center text-slate-300'> <span className='text-[#e74c3c] text-5xl'> <FaCampground></FaCampground> </span> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 
-        <ul>
-            <li className='flex gap-3 text-slate-300'> <span className='text-[#e74c3c]'><FaCheck></FaCheck></span> Refresing to get such a personal touch.</li>
-            <li className='flex gap-3 text-slate-300'> <span className='text-[#e74c3c]'><FaCheck></FaCheck></span> Duis aute irure dolor in reprehenderit in voluptate.</li>
-            <li className='flex gap-3 text-slate-300'> <span className='text-[#e74c3c]'><FaCheck></FaCheck></span> Velit esse cillum dolore eu fugiat nulla pariatur.</li>
+        <ul className='space-y-3'>
+            <li className='flex gap-3 text-slate-300'> <span className='rounded-full inline-block p-1 bg-[#e74c3c]'><FaCheck></FaCheck></span> Refresing to get such a personal touch.</li>
+            <li className='flex gap-3 text-slate-300'> <span className='rounded-full inline-block p-1 bg-[#e74c3c]'><FaCheck></FaCheck></span> Duis aute irure dolor in reprehenderit in voluptate.</li>
+            <li className='flex gap-3 text-slate-300'> <span className='rounded-full inline-block p-1 bg-[#e74c3c]'><FaCheck></FaCheck></span> Velit esse cillum dolore eu fugiat nulla pariatur.</li>
         </ul>
             </div>
         </div>
---
+++ src/Pages/Homepage/VideoSection/VideoSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoSection = () => {
+    return (
+        <div className='h-[40vh]'>
+            
+        </div>
+    );
+};
+
+export default VideoSection;
