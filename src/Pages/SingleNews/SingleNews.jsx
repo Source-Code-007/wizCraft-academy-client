2f378c71cbba17065119511b88228900df4f56cc
Source-Code-007 <utsho926@gmail.com>
2023-06-17 18:40:50 +0600

    single news page updated

--- src/Pages/SingleNews/SingleNews.jsx
+++ src/Pages/SingleNews/SingleNews.jsx
@@ -52,9 +52,9 @@ const SingleNews = () => {
             </div>
 
             <div className="grid grid-cols-12 my-container py-14">
-                <div className="col-span-8">
+                <div className="col-span-8 space-y-5">
 
-                    <div className={`card card-compact shadow-xl text-slate-200 overflow-hidden cmn-gradient-one`}>
+                    <div className={`card card-compact shadow-inner shadow-slate-300 text-slate-200 overflow-hidden`}>
                         <div className="relative group overflow-hidden">
                             <img src={img} alt={newsTitle} className="h-80 w-full rounded-t group-hover:scale-105 transition duration-500 rounded" />
                             <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 transition-opacity duration-300 hover:opacity-50"></div>
@@ -67,10 +67,40 @@ const SingleNews = () => {
                             <p>Tags {tags.map((tag, ind) => <button key={ind} className="cmn-btn-two mx-3 !bg-[#063a92]">{tag}</button>)}</p>
                         </div>
                     </div>
+
+                    <div className="space-y-4 !mt-28">
+                        <h3 className="font-bold text-3xl">{comments?.length} comments to "{newsTitle}" </h3>
+                        {
+                            comments.length && comments?.map((comment, ind) => {
+                                return <div key={ind} className="shadow-inner shadow-slate-300 rounded p-5 px-16 space-y-4" >
+                                    <div className="flex justify-between gap-5">
+                                        <h2 className="flex gap-2 items-center text-3xl font-bold">{comment?.commentAuthor} <span className="text-[#e74c3c] font-normal text-lg">{new Date(comment?.commentDate).toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" })}</span></h2>
+                                        <button className="cmn-btn-one">Reply</button>
+                                    </div>
+                                    <p className="text-slate-300">{comment?.commentText}</p>
+                                </div>
+                            })
+                        }
+                    </div>
+
+                    <div className="!my-24 p-5 shadow-inner shadow-slate-300">
+                        <h2 className="font-bold text-4xl my-5">Leave a comment!</h2>
+                        <form className="space-y-4">
+                            <div className="flex gap-5">
+                                <input type="text" placeholder="Name" className="my-inp" />
+                                <input type="email" placeholder="email" className="my-inp" />
+                            </div>
+                            <textarea className="my-inp w-full h-60" name="comment" placeholder="Your comment here"></textarea>
+                            <button className="cmn-btn-one ">Post comment</button>
+                        </form>
+                    </div>
+
                 </div>
+
                 <div className="col-span-4">
 
                 </div>
+
             </div>
 
 
