afdb25b94cd178aa78148204273cb3f39193193f
Source-Code-007 <utsho926@gmail.com>
2023-06-20 15:29:14 +0600

    refactor

--- src/HelpingComponent/CommonSectionTitle.jsx
+++ src/HelpingComponent/CommonSectionTitle.jsx
@@ -1,10 +1,15 @@
+import MyMotion from "./MyMotion";
+
 /* eslint-disable react/prop-types */
-const CommonSectionTitle = ({title, subtitle}) => {
+
+const CommonSectionTitle = ({ title, subtitle }) => {
     return (
-        <div className="py-14 mx-auto w-5/6 md:w-4/6 xl:w-3/6 space-y-4 text-center relative z-50">
-            <h2 className="font-bold text-5xl">{title}</h2>            
-            <p className="text-lg text-[#e74c3c]">{subtitle}</p>            
-        </div>
+        <MyMotion y={-100}>
+            <div className="py-14 mx-auto w-5/6 md:w-4/6 xl:w-3/6 space-y-4 text-center relative z-50 ">
+                <h2 className="font-bold text-5xl">{title}</h2>
+                <p className="text-lg text-[#e74c3c]">{subtitle}</p>
+            </div>
+        </MyMotion>
     );
 };
 
